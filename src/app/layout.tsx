4e5c83c5ee2cf60c2de43436de47ff6ad8fd2fdf
Sohag Jabed <sohagjabed10@gmail.com>
2024-10-02 14:34:46 +0530

    Custom sigin and signup pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
 
 import { WatchlistProvider } from "@/components/WatchlistProvider";
 
@@ -20,7 +21,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        baseTheme: dark,
+      }}
+    >
       <html lang="en" className={`${GeistSans.variable}`}>
         <WatchlistProvider>
           <body className="dark mb-24 md:mb-0 pb-20 md:pb-0">
