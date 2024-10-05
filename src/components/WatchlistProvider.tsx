a887835c5a7ba46ab83cd94891b5de7c6750c211
Sohag Jabed <sohagjabed10@gmail.com>
2024-10-05 23:44:09 +0530

    refactor

--- src/components/WatchlistProvider.tsx
+++ src/components/WatchlistProvider.tsx
@@ -1,9 +1,14 @@
-'use client';
-import React, { createContext, useContext, useState, useEffect } from 'react';
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { useAuth } from "@clerk/nextjs";
-import { type Watchlist, type WatchlistMedia, type WatchlistResponse, type MediaType } from "@/lib/interfaces";
+import {
+  type Watchlist,
+  type WatchlistMedia,
+  type WatchlistResponse,
+  type MediaType,
+} from "@/lib/interfaces";
 
-type MovieStatus = 'not watched' | 'watched';
+type MovieStatus = "not watched" | "watched";
 
 interface WatchlistContextType {
   watchlist: WatchlistMedia[];
@@ -15,16 +20,20 @@ interface WatchlistContextType {
   updateStatus: (movieId: number, status: MovieStatus) => Promise<void>;
 }
 
-const WatchlistContext = createContext<WatchlistContextType | undefined>(undefined);
+const WatchlistContext = createContext<WatchlistContextType | undefined>(
+  undefined,
+);
 
-export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [watchlist, setWatchlist] = useState<WatchlistMedia[]>([]);
   const { userId } = useAuth();
 
   useEffect(() => {
     const fetchWatchlist = async () => {
       const res = await fetch(`/api/watchlist`);
-      const data = await res.json() as Watchlist;
+      const data = (await res.json()) as Watchlist;
       setWatchlist(data.watchlist);
     };
 
@@ -32,24 +41,27 @@ export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({ chi
   }, []);
 
   const addToWatchlist = async (mediaID: number, mediaType: MediaType) => {
-    const res = await fetch(`/api/watchlist?movie_id=${mediaID}&media_type=${mediaType}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const res = await fetch(
+      `/api/watchlist?movie_id=${mediaID}&media_type=${mediaType}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    });
-    const data = await res.json() as WatchlistResponse;
+    );
+    const data = (await res.json()) as WatchlistResponse;
     if (data.success) {
-    const movie: WatchlistMedia = {
-      id: mediaID,
-      watchlist_id: null,
-      movie_id: mediaID,
-      media_type: mediaType,
-      user_id: userId?.toString() ?? "",
-      status: "active",
-      added_at: new Date(),
-    }
-    setWatchlist((prevWatchlist) => [movie, ...prevWatchlist]);
+      const movie: WatchlistMedia = {
+        id: mediaID,
+        watchlist_id: null,
+        movie_id: mediaID,
+        media_type: mediaType,
+        user_id: userId?.toString() ?? "",
+        status: "active",
+        added_at: new Date(),
+      };
+      setWatchlist((prevWatchlist) => [movie, ...prevWatchlist]);
     } else {
       console.error(data.message);
     }
@@ -57,14 +69,16 @@ export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({ chi
 
   const removeFromWatchlist = async (movieId: number) => {
     const res = await fetch(`/api/watchlist?movie_id=${movieId}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
-    const data = await res.json() as WatchlistResponse;
+    const data = (await res.json()) as WatchlistResponse;
     if (data.success) {
-      setWatchlist((prevWatchlist) => prevWatchlist.filter((movie) => movie.movie_id !== movieId));
+      setWatchlist((prevWatchlist) =>
+        prevWatchlist.filter((movie) => movie.movie_id !== movieId),
+      );
     } else {
       console.error(data.message);
     }
@@ -75,31 +89,48 @@ export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({ chi
   };
 
   const updateStatus = async (movieId: number, status: MovieStatus) => {
-    const res = await fetch(`/api/watchlist?movie_id=${movieId}&status=${status}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
+    const res = await fetch(
+      `/api/watchlist?movie_id=${movieId}&status=${status}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    });
-    const data = await res.json() as WatchlistResponse;
+    );
+    const data = (await res.json()) as WatchlistResponse;
     if (data.success) {
-      setWatchlist((prevWatchlist) => prevWatchlist.map((movie) => {
-        if (movie.movie_id === movieId) {
-          return { ...movie, status: 'watched' };
-        }
-        return movie;
-      }));
+      setWatchlist((prevWatchlist) =>
+        prevWatchlist.map((movie) => {
+          if (movie.movie_id === movieId) {
+            return { ...movie, status: "watched" };
+          }
+          return movie;
+        }),
+      );
     } else {
       console.error(data.message);
     }
   };
 
   const isWatched = (movieId: number) => {
-    return watchlist.some((movie) => movie.movie_id === movieId && movie.status === 'watched');
+    return watchlist.some(
+      (movie) => movie.movie_id === movieId && movie.status === "watched",
+    );
   };
 
   return (
-    <WatchlistContext.Provider value={{ watchlist, setWatchlist, addToWatchlist, removeFromWatchlist, isInWatchlist, updateStatus, isWatched }}>
+    <WatchlistContext.Provider
+      value={{
+        watchlist,
+        setWatchlist,
+        addToWatchlist,
+        removeFromWatchlist,
+        isInWatchlist,
+        updateStatus,
+        isWatched,
+      }}
+    >
       {children}
     </WatchlistContext.Provider>
   );
@@ -108,7 +139,7 @@ export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({ chi
 export const useWatchlist = () => {
   const context = useContext(WatchlistContext);
   if (!context) {
-    throw new Error('useWatchlist must be used within a WatchlistProvider');
+    throw new Error("useWatchlist must be used within a WatchlistProvider");
   }
   return context;
 };
